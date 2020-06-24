f503833db7db9bad54456aae2b9ddb55dfe5119f
JCPurger <joao.macedo@spot-edu.com.br>
2020-06-24 19:52:29 -0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const socketIO = require('socket.io')
+
+const path = require('path')
+const PORT = process.env.PORT || 5000
+
+const server = express()
+  .use(express.static(path.join(__dirname, 'public')))
+  .set('views', path.join(__dirname, 'views'))
+  .set('view engine', 'ejs')
+  .get('/', (req, res) => res.render('pages/index'))
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+
+const io = socketIO(server);
+
+let messages = [];
+
+io.on('connection', socket => {
+    console.log(`Socket conectado: ${socket.id}`)
+
+    socket.emit('previousMessages', messages)
+
+    socket.on('sendMessage', data => {
+        messages.push(data)
+        socket.broadcast.emit('receivedMessage', data)
+    })
+})
