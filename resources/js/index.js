de6d22783bda696e8e6c167b9ca6a85d93ffca7d
JCPurger <joao.macedo@spot-edu.com.br>
2020-06-29 23:45:21 -0300

    Dependencies front to npm and jquery removed

--- resources/js/index.js
+++ resources/js/index.js
@@ -1,17 +1,22 @@
 import '../css/style.scss';
 
+import io from 'socket.io-client';
+
 var socket = io(window.location.href);
 
+const authorEl = document.querySelector('input[name=username]');
+const messageEl = document.querySelector('input[name=message]');
+
 function renderMessage(message) {
-    $('.messages')
-        .append(`
+    document.querySelector('.messages')
+        .insertAdjacentHTML('beforeend',`
             <div class="message" <strong>${message.author}</strong>:  ${message.message}<div>
         `);
 }
 
 socket.on('previousMessages', function(messages) {
-    for(message of messages) {
-        renderMessage(message);
+    for(let message of messages) {
+        renderMessage(message);''
     }
 });
 
@@ -23,18 +28,19 @@ socket.on('clearAll', function() {
     $('.messages .message').remove();
 });
 
-$('#chat').submit(function (e) {
+document.querySelector('#chat').addEventListener("submit", function (e) {
     e.preventDefault();
-
-    var author = $('input[name=username]').val();
-    var message = $('input[name=message]').val();
+    
+    let author = authorEl.value;
+    let message = messageEl.value;
 
     if(author.length && message.length) {
-        $('input[name=message]').val('');
         var messageObject = {
             author: author,
             message: message
         };
+
+        messageEl.value = '';
     }
 
     renderMessage(messageObject);
